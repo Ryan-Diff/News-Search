601031202307971534766b4a799259636d99c47e
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-10-21 14:28:12 -0700

    Article

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Article from '../articles/Article';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Article
+    title='Stocks Chop On Stimulus Slop, Bitcoin Jumps, Dollar Dumps'
+    author='Tyler Durden'
+    description='Stocks Chop On Stimulus Slop, Bitcoin Jumps, Dollar Dumps\n\n Tyler Durden\n \nWed, 10/21/2020 - 16:00\n\n The algos were busy today... buying on every mention of the words \"hope\" or \"optimism\" and selling at technical resistance as once again nothing happened...\r\n…' />;
 }
---
+++ src/services/news-api.js
@@ -0,0 +1,13 @@
+export const getNews = () => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${search}&apiKey=${process.env.API_KEY}`, {
+        header: {
+            Origin: null
+        }
+    })
+    .then(res => res.json())
+    .then(json => json.articles.map(article => ({
+        title: article.title,
+        author: article.author,
+        description: article.description
+    })));
+};
